2bed519387d9f5a9afae5f008b1f6a9432626d2c
jonathan carlos and jake nagel <jake@>
2020-01-22 13:09:42 -0800

    add for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
   $("form#wordPuzzle").submit(function(event) {
-    var userInput = $("input#input1").val();
-    var vowels = $()
-
-    
-    
-    $(".input1").text(userInput);
-
     event.preventDefault();
+    var userInput = $("input#input").val();
+    var vowels = ["a", "e", "i", "o", "u"]
+    var newSentence = userInput.split("");
+
+    for (var word = 0; word < newSentence.length; word += 1) {
+      vowels.forEach(function(vowel) {
+        if (newSentence[word] === vowel)
+          newSentence[word] = "-"
+      })
+    }
+    
+    var displayedSentence = newSentence.join("");
+    $(".input").text(displayedSentence);
+
     
   });
 });
